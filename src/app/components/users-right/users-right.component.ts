a93619972b86d50c4b818ce85d0d118d6d437b9a
zee-creato <85754092+zee-creato@users.noreply.github.com>
2023-03-01 17:43:32 +0000

    add

--- src/app/components/users-right/users-right.component.ts
+++ src/app/components/users-right/users-right.component.ts
@@ -1,15 +1,62 @@
 import { Component, OnInit } from '@angular/core';
+import { Model } from '../users-right/model';
+import { Models } from '../users-right/models';
+import { Camera } from './cameras/camera';
+import { Cameras } from './cameras/cameras';
 
 @Component({
   selector: 'app-users-right',
   templateUrl: './users-right.component.html',
-  styleUrls: ['./users-right.component.scss']
+  styleUrls: ['./users-right.component.scss'],
 })
 export class UsersRightComponent implements OnInit {
+  isActive: boolean = false;
+  users: Model[] = Models;
+  userName: string = 'John';
+  userImage: string = 'assets/user.jpeg';
+  cameraT:boolean=false;
+  cameras:Camera[]=Cameras;
+  cameraActive:boolean;
+  cameraImage:string='assets/garden.jpg'
+  cameraName:string='garden';
+  dotActive:boolean=true;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  user() {
+    this.isActive = !this.isActive;
   }
 
+  userChange(item: Model) {
+    console.log(item);
+    this.userName = item.name;
+    this.userImage = item.image;
+  }
+
+  camera(){
+    console.log('Dziala')
+    this.cameraT =! this.cameraT;
+  }
+
+
+  cameraC(item:Camera){
+    console.log(item)
+    this.cameraImage = item.image;
+    this.cameraName = item.name;
+    this.cameraActive = item.active;
+    this.dotActive = item.active;
+  }
+
+
+  startRecord(item:Camera){
+    console.log('Nagrywa');
+    item.active=true;
+  }
+  stopRecording(item:Camera){
+    item.active=false;
+
+  }
+
+  ngOnInit(): void {
+
+  }
 }
